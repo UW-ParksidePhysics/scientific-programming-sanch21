ecbb4c9be644cc7d7e0bfebabb1e944f98dd56d2
Sanch21 <98779387+Sanch21@users.noreply.github.com>
2022-03-16 13:46:09 -0500

    Update gaussian_plot.m

---
+++ matlab/gaussian_plot.m
@@ -0,0 +1,12 @@
+x = [0:1:10]
+mu = 5
+e = 2.718
+y1 = (1/((0.5)*sqrt(2*pi)))*e.^-((x-mu).^2)/(2*(0.5).^2)
+y2 = (1/((1.0)*sqrt(2*pi)))*e.^-((x-mu).^2)/(2*(1.0).^2)
+y3 = (1/((1.5)*sqrt(2*pi)))*e.^-((x-mu).^2)/(2*(1.5).^2)
+function_values = []
+sigma = 0.5:0.5:1.5
+plot(x,y1,"-", x,y2,"--", x,y3,":")
+xlabel('xaxis')
+ylabel('phi(x-5,sigma)')
+legend('sigma(0.5)','sigma(1.0)','sigma(1.5)')
