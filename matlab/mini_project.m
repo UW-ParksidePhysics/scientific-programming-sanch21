1fc12aef70fa040f6579a447b806f2126a2f5089
Sanch21 <98779387+Sanch21@users.noreply.github.com>
2022-04-07 14:54:09 -0500

    Update mini_project.m

--- matlab/mini_project.m
+++ matlab/mini_project.m
@@ -4,7 +4,14 @@
 % Simulation parameters
 %	These are values particular to the simulation 
 %	that do not change later in the script
-
+% Intial velocity (v0) = 0 m/s
+% Intial height   (h0) = 5 m
+% Gravity          (g) = 9.81 m/s
+% Height of pulley (H) = 10 m 
+% Radius of pulley (R) = 1 m
+% Length of rope   (L) = 15 m 
+% Masses          (m1) = 2 kg
+%                 (m2) = 5 kg
 % Computed parameters (from simulation parameters)
 %	These are values that do not change later in the script
 %	and are calculated from formulas using the simulation parameters
@@ -13,7 +20,14 @@
 %	data read-in
 %	simulation solution 
 %	visualization
-
+m1 = 2
+m2 = 5
+g = -9.81
+v0 = 2
+a = ((m2-m1).*g)/(m1+m2)
+t = (0:0.1:10)
+h = v0.*t + 0.5.*a*t.^2
+plot(t,h) 
 % Function definitions for simulation solution & visualization
 %	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
 
