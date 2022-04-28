3712837eb7373d833bd5c2d95c81551cccd68835
Sanch21 <98779387+Sanch21@users.noreply.github.com>
2022-04-28 14:52:47 -0500

    Update birthday_vector.py

--- python/birthday_vector.py
+++ python/birthday_vector.py
@@ -1,4 +1,5 @@
 from vpython import *
+
 # GlowScript 3.0 VPython
 
 # Written by Ruth Chabay, licensed under Creative Commons 4.0.
@@ -9,19 +10,20 @@ from vpython import *
 scene.background = color.white
 scene.width = 600
 scene.height = 600
-scene.forward = vector(-.5,-.3,-1)
+scene.forward = vector(-.5, -.3, -1)
 
-scene.caption= """To rotate "camera", drag with right button or Ctrl-drag.
+scene.caption = """To rotate "camera", drag with right button or Ctrl-drag.
 To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
   On a two-button mouse, middle is left + right.
 To pan left/right and up/down, Shift-drag.
 Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
 
-xaxis=cylinder(color=vector(1,0,0), pos=vector(0,0,0), axis=vector(10,0,0), radius=0.3)
-xlbl=label(pos=vector(11,0,0), text="X", color=color.red, opacity=0, height=30, box=0)
-yaxis=cylinder(color=color.green, pos=vector(0,0,0), axis=vector(0,10,0), radius=0.3)
-ylbl=label(pos=vector(0,11,0), text="Y", color=color.green, opacity=0, height=30, box=0)
-zaxis=cylinder(color=color.blue, pos=vector(0,0,0), axis=vector(0,0,10), radius=0.3)
-xlbl=label(pos=vector(0,0,11), text="Z", color=color.blue, opacity=0, height=30, box=0)
+x_axis = cylinder(color=vector(1, 0, 0), pos=vector(0, 0, 0), axis=vector(10, 0, 0), radius=0.3)
+x_lbl = label(pos=vector(11, 0, 0), text="x", color=color.red, opacity=0, height=30, box=0)
+y_axis = cylinder(color=color.green, pos=vector(0, 0, 0), axis=vector(0, 10, 0), radius=0.3)
+y_lbl = label(pos=vector(0, 11, 0), text="y", color=color.green, opacity=0, height=30, box=0)
+z_axis = cylinder(color=color.blue, pos=vector(0, 0, 0), axis=vector(0, 0, 10), radius=0.3)
+x_lbl = label(pos=vector(0, 0, 11), text="z", color=color.blue, opacity=0, height=30, box=0)
 
-r = arrow(pos=vector(0,0,0), axis = vector(2,10,7), color=color.white, shaftwidth=0.5)
+r = arrow(pos=vector(0, 0, 0), axis=vector(10, 26, 99), color=color.yellow, shaftwidth=0.5)
+r_lbl = label(pos=vector(10, 26, 99), text="10x+26y+99Z", color=color.black, opacity=0, height=30, box=0)
