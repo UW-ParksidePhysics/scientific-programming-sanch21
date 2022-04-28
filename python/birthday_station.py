2970debba8d2032bb0acb4432241ff4ce32b5c0b
Sanch21 <98779387+Sanch21@users.noreply.github.com>
2022-04-28 14:50:58 -0500

    Update birthday_station.py

--- python/birthday_station.py
+++ python/birthday_station.py
@@ -1,4 +1,5 @@
 from vpython import *
+
 # GlowScript 3.0 VPython
 
 # Trajectory of a ball thrown on a rotating space station with 'artificial gravity'
@@ -18,52 +19,57 @@ from vpython import *
 gotv = False
 v = vector(0, 0, 0)
 pause = False
-rotation = 0
+Frank_birthdate = 26
+Adrian_birthdate = 30
+
+rotation = Frank_birthdate / Adrian_birthdate
 drag = False
+
 
 class spacestation:
     def __init__(self, whichcanvas):
-        self.N = 50 # number of boxes used to create the ring-shaped space station
-        self.R = 10 # inner radius of space station
-        self.h = 2 # height of release of ball above the "floor" of the space station
+        self.N = 50  # number of boxes used to create the ring-shaped space station
+        self.R = 10  # inner radius of space station
+        self.h = 2  # height of release of ball above the "floor" of the space station
         self.canvas = whichcanvas
         whichcanvas.select()
-        self.person = cylinder(pos=vector(0, -self.R, 0), axis=vector(0, self.h, 0), 
-                        size=vector(self.h, 2 * 0.1, 2 * 0.1)) 
-        
-        thick = 0.5 # thickness of space station
+        self.person = cylinder(pos=vector(0, -self.R, 0), axis=vector(0, self.h, 0),
+                               size=vector(self.h, 2 * 0.1, 2 * 0.1))
+
+        thick = 0.5  # thickness of space station
         dtheta = 2 * pi / self.N
-        paint = color.red
-        red = True
+        paint = color.green
+        green = True
         boxes = [self.person]
-        
+
         for i in range(self.N):
             theta = i * dtheta
             b = box(pos=(self.R + thick / 2) * vector(cos(theta), sin(theta), 0),
                     size=vector(thick, 2 * (self.R + thick) * sin(dtheta / 2), thick))
-            if red:
-              b.color = color.red
-              red = False
+            if green:
+                b.color = color.green
+                green = False
             else:
-              b.color = color.blue
-              red = True
+                b.color = color.yellow
+                green = True
             b.rotate(angle=theta, axis=vector(0, 0, 1))
             boxes.append(b)
-        
+
         self.hull = compound(boxes)
-        
+
         self.ball = sphere(pos=self.person.pos + self.person.axis,
-                    color=color.cyan, size=2 * 0.2 * vector(1, 1, 1))
-        
+                           color=color.cyan, size=2 * 0.2 * vector(1, 1, 1))
+
         self.trail = attach_trail(self.ball, radius=0.1 * self.ball.size.x, pps=10, retain=500)
         self.reset()
-        
+
     def reset(self):
         global rotation
         self.hull.rotate(angle=-rotation, axis=vector(0, 0, 1), origin=vector(0, 0, 0))
         self.ball.pos = self.person.pos + self.person.axis
         self.trail.clear()
-        rotation =  0
+        rotation = 0
+
 
 def bind_mouse(station, vector1, vector2):
     global pause
@@ -72,7 +78,7 @@ def bind_mouse(station, vector1, vector2):
 
     def down():
         global drag, gotv, v
-        
+
         def set_v():
             vector1.axis = s.mouse.pos - vector1.pos
             vector2.axis = vector1.axis
@@ -84,10 +90,11 @@ def bind_mouse(station, vector1, vector2):
 
         set_v()
         drag = True
+
         def move():
-          global drag, pause
-          if pause: return
-          if drag: set_v()
+            global drag, pause
+            if pause: return
+            if drag: set_v()
 
         def up():
             global drag, gotv, v, pause
@@ -104,11 +111,12 @@ def bind_mouse(station, vector1, vector2):
             else:
                 v = vector2.axis / scalefactor
             gotv = True
-            
+
         s.bind("mousemove", move)
         s.bind("mouseup", up)
-            
+
     s.bind("mousedown", down)
+
 
 scene1 = canvas(width=430, height=400, align='left', userspin=False, userzoom=False)
 scene2 = canvas(width=430, height=400, align='left', userspin=False, userzoom=False)
@@ -124,24 +132,24 @@ deltat = 0.001 * 2 * pi / omega
 v0 = omega * (station1.R - station1.h)
 scalefactor = 5 / (omega * station1.R)
 v1 = arrow(canvas=scene1, pos=station1.ball.pos, color=color.green,
-            axis=vector(0, 0, 0), shaftwidth=0.4, visible=False)
+           axis=vector(0, 0, 0), shaftwidth=0.4, visible=False)
 
 v2 = arrow(canvas=scene2, pos=station2.ball.pos, color=color.green,
-            axis=vector(0, 0, 0), shaftwidth=0.4, visible=False)
+           axis=vector(0, 0, 0), shaftwidth=0.4, visible=False)
 
 instruct1 = label(canvas=scene1, pos=vector(0, station1.R / 2, 0),
                   text="Drag initial velocity in the inertial frame",
-                  visible= False)
+                  visible=False)
 
 instruct2 = label(canvas=scene2, pos=vector(0, station2.R / 2, 0),
                   text="Or drag initial velocity relative to rotating space station",
                   visible=False)
 
 click1 = label(canvas=scene1, pos=vector(0.8 * station1.R, -1 * station1.R, 0),
-                  text="Click to\nstart over", visible=False)
+               text="Click to\nstart over", visible=False)
 
 click2 = label(canvas=scene2, pos=vector(0.8 * station2.R, -1 * station2.R, 0),
-                  text="Click to\nstart over", visible=False)
+               text="Click to\nstart over", visible=False)
 
 bind_mouse(station1, v1, v2)
 bind_mouse(station2, v2, v1)
@@ -154,26 +162,26 @@ while True:
     v1.visible = v2.visible = True
     instruct1.visible = instruct2.visible = True
     while True:
-        rate (50)
+        rate(50)
         if gotv: break
     v1.visible = v2.visible = False
     instruct1.visible = instruct2.visible = False
-    r = vector(station1.ball.pos) # initialize r without making it equivalent to ball.pos
+    r = vector(station1.ball.pos)  # initialize r without making it equivalent to ball.pos
     t = 0
     while True:
-        rate(0.5/deltat) # slow down the plotting
+        rate(0.5 / deltat)  # slow down the plotting
         rotation += omega * deltat
-        
+
         station1.ball.rotate(angle=omega * deltat, axis=vector(0, 0, 1), origin=vector(0, 0, 0))
-        
+
         station1.hull.rotate(angle=omega * deltat, axis=vector(0, 0, 1), origin=vector(0, 0, 0))
-        
-        r = r + v * deltat # update the actual position of the ball (in inertial frame)
+
+        r = r + v * deltat  # update the actual position of the ball (in inertial frame)
         station1.ball.pos = r
         newr = vector(r)
         station2.ball.pos = newr.rotate(angle=-omega * t, axis=vector(0, 0, 1))
-    
-        if mag(station1.ball.pos) >= station1.R: # if ball hits floor, make it stick there
+
+        if mag(station1.ball.pos) >= station1.R:  # if ball hits floor, make it stick there
             direction = norm(station1.ball.pos)
             station1.ball.pos = station1.R * direction
             direction = norm(station2.ball.pos)
@@ -187,4 +195,6 @@ while True:
         if not pause: break
     gotv = False
     click1.visible = click2.visible = False
+
+
 
