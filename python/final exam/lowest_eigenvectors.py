6a0fba14509ea206b22ac40a163f24754a33d4b4
adria <sanch077@rangers.uwp.edu>
2022-05-09 15:08:29 -0500

    mes

--- python/final exam/lowest_eigenvectors.py
+++ python/final exam/lowest_eigenvectors.py
@@ -3,6 +3,14 @@ import numpy as np
 
 
 def lowest_eigenvectors(square_matrix, number_of_eigenvectors=3):
+    '''
+
+    :param square_matrix: ndarray, shape(M, M)
+    Matrix to be characterized. Must be a square matrix of M rows and M columns where M is >=1.
+    :param number_of_eigenvectors: int, optional
+    Number of eigenvectors K with eigenvalues to return.  Default is 3.
+    :return:
+    '''
     m, n = square_matrix.shape
     if m != n:
         raise IndexError('not square')
