4a4d0908d25682b144dccc1e295c62aef21e533c
adria <sanch077@rangers.uwp.edu>
2022-05-09 15:11:56 -0500

    mes

--- python/final exam/fit_curve_array.py
+++ python/final exam/fit_curve_array.py
@@ -3,6 +3,18 @@ import numpy as np
 
 
 def fit_curve_array(quadratic_coefficients, minimum_x, maximum_x, data_points=50):
+    '''
+
+    :param quadratic_coefficients: ndarray, shape (3,)
+    Quadratic polynomial coefficients, ordered quadratic term first, then linear term, and constant term last.
+    :param minimum_x:float
+    Starting value for the fit_curve array
+    :param maximum_x:float
+    Ending value for the fit curve array
+    :param data_points: int, optional
+    Number of points N to return for final fit curve. Default is 100.
+    :return:
+    '''
     if maximum_x < minimum_x:
         raise ArithmeticError
     if data_points <= 2:
